5961e4cca1f0f5413f9461a2ebd9d414a1f356fe
Stepan Demianenko <stepan.demyanenko30@gmail.com>
2025-08-19 13:55:09 +0100

    Add - New exercise section GYM design only

--- app/src/main/java/com/example/fitnessapp/ExerciseActivity.java
+++ app/src/main/java/com/example/fitnessapp/ExerciseActivity.java
@@ -8,73 +8,54 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExerciseActivity extends AppCompatActivity {
-    //Bottom Panel Buttons
-    private Button profile;
-    private Button home;
-    private Button exercise;
-    private Button settings;
-
-    //Exercise Buttons
-    private Button btn_running;
-    private Button btn_hiking;
-    private Button btn_cycling;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        home = findViewById(R.id.btn_home);
-        exercise = findViewById(R.id.btn_exercise);
-        profile = findViewById(R.id.btn_profile);
-        settings = findViewById(R.id.btn_settings);
+        Button home = findViewById(R.id.btn_home);
+        Button exercise = findViewById(R.id.btn_exercise);
+        Button profile = findViewById(R.id.btn_profile);
+        Button settings = findViewById(R.id.btn_settings);
 
-        btn_running = findViewById(R.id.btn_runningimage);
-        btn_hiking = findViewById(R.id.btn_hikingimage);
-        btn_cycling = findViewById(R.id.btn_cyclingimage);
+        Button btn_running = findViewById(R.id.btn_runningimage);
+        Button btn_hiking = findViewById(R.id.btn_hikingimage);
+        Button btn_cycling = findViewById(R.id.btn_cyclingimage);
+        Button btn_gym = findViewById(R.id.btn_gym_image);
 //Bottom Panel ->
-        home.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
+        home.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, HomeActivity.class);
+            startActivity(intent);
         });
-        profile.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
+        profile.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, ProfileActivity.class);
+            startActivity(intent);
         });
-        exercise.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, ExerciseActivity.class);
-                startActivity(intent);
-            }
+        exercise.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, ExerciseActivity.class);
+            startActivity(intent);
         });
-        settings.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, SettingsActivity.class);
-                startActivity(intent);
-            }
+        settings.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, SettingsActivity.class);
+            startActivity(intent);
         });
 //end
 //ExerciseButtons
-        btn_running.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, RunningJoggingActivity.class);
-                startActivity(intent);
-            }
+        btn_running.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, RunningJoggingActivity.class);
+            startActivity(intent);
         });
-        btn_hiking.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, HikingActivity.class);
-                startActivity(intent);
-            }
+        btn_hiking.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, HikingActivity.class);
+            startActivity(intent);
         });
-        btn_cycling.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(ExerciseActivity.this, RunningJoggingActivity.class);
-                startActivity(intent);
-            }
+        btn_cycling.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, RunningJoggingActivity.class);
+            startActivity(intent);
+        });
+        btn_gym.setOnClickListener(v -> {
+            Intent intent = new Intent(ExerciseActivity.this, RunningJoggingActivity.class);
+            startActivity(intent);
         });
     }
 }
