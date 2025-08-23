68a2b581e4273659624e92b6a62e400ae8fd9242
Stepan Demianenko <stepan.demyanenko30@gmail.com>
2025-08-23 15:38:55 +0100

    Implement - StopWatch - Start/Pause/Stop functionalities

--- app/src/main/java/com/example/fitnessapp/workouts/GymActivity.kt
+++ app/src/main/java/com/example/fitnessapp/workouts/GymActivity.kt
@@ -1,17 +1,18 @@
 package com.example.fitnessapp.workouts
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,18 +22,32 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentDataType
+import androidx.compose.ui.autofill.ContentDataType.Companion.Text
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.graphics.alpha
+import androidx.room.util.TableInfo
 import com.example.fitnessapp.ExerciseActivity
 import com.example.fitnessapp.HomeActivity
 import com.example.fitnessapp.ProfileActivity
@@ -51,6 +66,10 @@ class GymActivity : ComponentActivity() {
 @Preview
 fun GymApp() {
     val context = LocalContext.current
+    val formattedTime = "00:00" // replace later with real state
+    // Column for stopwatch + set time inputs
+    val stopWatch = remember { StopWatch() }
+    // Background Image --------------------------------------------------------------------
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -61,65 +80,117 @@ fun GymApp() {
                 .fillMaxSize(),
             contentScale = ContentScale.Crop // scales and crops to fill the container
         )
+    }
+
+    // StopWatch Container -----------------------------------------------------------------
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Box(
             modifier = Modifier.fillMaxWidth()
                 .wrapContentHeight(),
             contentAlignment = Alignment.Center
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.timercircleset),
-                contentDescription = null,
+        ) {
+            Box(
                 modifier = Modifier
-                    .padding(top = 150.dp)
-                    .size(250.dp), // shortcut for width & height
-            )
+                    .padding(top = 100.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.timercircleset),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(250.dp), // shortcut for width & height
+                )
+                Button(
+                    onClick = {stopWatch.start()},
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(250.dp)
+                        .alpha(0F),
+                ) { }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = stopWatch.formattedTime,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        color = Color(0xFF4F2912)
+                    )
+                }
+            }
         }
-
-        Column(
-            modifier = Modifier
-                .align(Alignment.Center) // or BottomCenter if you want it at bottom
-                .padding(top = 350.dp)   // adjust position relative to circle
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.set_time_body),
-                contentDescription = null,
+        Spacer(Modifier.height(30.dp))
+        Box(){
+            Row(
                 modifier = Modifier
-                    .padding(top = 0.dp)
-                    .height(50.dp)
-                    .width(300.dp)
-            )
-            Spacer(modifier = Modifier.height(15.dp))
-            Image(
-                painter = painterResource(id = R.drawable.set_time_body),
-                contentDescription = null,
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                Arrangement.Center
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.stopwatch_pause),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp), // shortcut for width & height
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.stopwatch_stop),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp), // shortcut for width & height
+                )
+            }
+            Row(
                 modifier = Modifier
-                    .padding(top = 0.dp)
-                    .height(50.dp)
-                    .width(300.dp)
-            )
-            Spacer(modifier = Modifier.height(15.dp))
-            Image(
-                painter = painterResource(id = R.drawable.set_time_body),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 0.dp)
-                    .height(50.dp)
-                    .width(300.dp)
-            )
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                Arrangement.Center
+            ) {
+                Button(
+                    onClick = {stopWatch.pause()},
+                    modifier = Modifier
+                        .size(50.dp)
+                        .alpha(0F),
+                ) { }
+                Button(
+                    onClick = {stopWatch.reset()},
+                    modifier = Modifier
+                        .size(50.dp)
+                        .alpha(0F),
+                ) { }
+            }
+        }
+        Spacer(Modifier.height(20.dp))
+        // SetTime Container ----------------------------------------------------------------
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                repeat(3) {
+                    Image(
+                        painter = painterResource(id = R.drawable.set_time_body),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(50.dp)
+                            .width(300.dp),
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+            }
         }
     }
 
-    Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center // centers children by default
-    ){
-
-
-    }
-
+    // BottomPanel Container ---------------------------------------------------------------
     Box(
         modifier = Modifier
             .fillMaxSize()
---
+++ app/src/main/java/com/example/fitnessapp/workouts/StopWatch.kt
@@ -0,0 +1,67 @@
+package com.example.fitnessapp.workouts
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+import kotlinx.coroutines.*
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class StopWatch {
+    var formattedTime by mutableStateOf("00:00")
+
+    private var coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var isActive = false
+
+    private var timeMillis = 0L
+    private var lastTimeStamp = 0L
+    private var job: Job? = null
+
+    fun start(){
+        if(isActive) return
+
+        lastTimeStamp = System.currentTimeMillis()
+        isActive = true
+        job = CoroutineScope(Dispatchers.Main).launch {
+            lastTimeStamp = System.currentTimeMillis()
+            this@StopWatch.isActive = true
+            while(isActive){
+                val now = System.currentTimeMillis()
+                timeMillis += now - lastTimeStamp
+                lastTimeStamp = now
+                formattedTime = formatTime(timeMillis)
+                delay(50L) // UI-friendly update interval
+            }
+        }
+    }
+
+    fun pause() {
+        if (!isActive) return
+
+        // Update the elapsed time before stopping
+        val now = System.currentTimeMillis()
+        timeMillis += now - lastTimeStamp
+        isActive = false
+        job?.cancel()
+    }
+    fun reset() {
+        job?.cancel()
+        timeMillis = 0L
+        lastTimeStamp = 0L
+        formattedTime = "00:00"
+        isActive = false
+    }
+
+    private fun formatTime(timeMillis: Long) : String{
+        val minutes = (timeMillis / 1000) / 60
+        val seconds = (timeMillis / 1000) % 60
+        return String.format("%02d:%02d", minutes, seconds)
+    }
+
+}
